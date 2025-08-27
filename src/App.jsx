69869c6afafdb794854a39e63fe374dc7027e808
LiamJL <paledeepened@gmail.com>
2025-08-27 10:43:59 +0100

    Implement Header component: include HeaderTop and Navbar components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import Header from "./components/Layout/Header"
+import Store from "./pages/Store"
+
 function App() {
 
   return (
     <>
-      
+      <Header />
     </>
   )
 }
---
+++ src/components/Layout/Navbar.jsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+    return (
+        <nav>
+            <ul>
+                <li><a href="#">All Products</a></li>
+                <li><a href="#">Laptops</a></li>
+                <li><a href="#">Tablets</a></li>
+                <li><a href="#">Smartphones</a></li>
+                <li><a href="#">Accessories</a></li>
+            </ul>
+        </nav>
+    )
+}
