8232557b0c92ef8bfde8ebd761bf3603f76b2a62
LiamJL <paledeepened@gmail.com>
2025-09-06 10:17:46 +0100

    Position app components

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import Background from './components/Background/Background';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Landing from './pages/Landing/Landing';
+import styles from './App.module.css'
 
 function App() {
 
   return (
-    <div>
+    <div className={styles.app}>
       <Background />
       <Header />
-      <Landing />
+      <div className={styles.pageContainer}>
+        <Landing />
+      </div>
       <Footer />
     </div>
   )
