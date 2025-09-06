ca59499e180642bad86c82c6c6dfee400d5c0109
LiamJL <paledeepened@gmail.com>
2025-09-06 08:03:44 +0100

    FInalise scaffolding for app design

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import Header from "./components/Header/Header"
-import Store from "./pages/Store"
+import Background from './components/Background/Background';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Landing from './pages/Landing/Landing';
 
 function App() {
 
   return (
-    <>
+    <div>
+      <Background />
       <Header />
-    </>
+      <Landing />
+      <Footer />
+    </div>
   )
 }
 
---
+++ src/pages/Landing/Landing.jsx
@@ -0,0 +1,12 @@
+import Hero from '../../components/Hero/Hero';
+
+function Landing() {
+    return (
+        <div>
+            <h1>Landing</h1>
+            <Hero />
+        </div>
+    );
+}
+export default Landing;
+
