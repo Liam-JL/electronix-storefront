98b508a655f8075c3e6c0d1f6c262b943910363a
LiamJL <paledeepened@gmail.com>
2025-09-08 11:51:16 +0100

    Create Logo component and add to Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,21 @@
 import { IoSearch as SearchIcon, IoCartOutline as CartIcon } from "react-icons/io5";
+import styles from './Header.module.css'
 
 function Header() {
     return (
         <header>
-            <h1>Header</h1>
+            <Logo />        
         </header>
     );
+}
+
+function Logo() {
+    return (
+        <span className={styles.logo}>
+            <img src="../../../public/logoipsum-light.svg" alt="" />
+            <h1>Electronix</h1>
+        </span>
+    )
 }
 
 export default Header;
