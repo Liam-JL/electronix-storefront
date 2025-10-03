5861be279c5aa97053dced7f0a96500c855da0c3
LiamJL <paledeepened@gmail.com>
2025-10-03 11:53:26 +0100

    Add component to scroll to top of window on navigations

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
     <div className={styles.app}>
       <ProductsProvider>
         <CartProvider>
+          <ScrollToTop />
           <BrowserRouter>
             <Header />
             <PageContainer />
